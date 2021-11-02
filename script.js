c491d3eef5fa8f9aa966c3d93bcf86d009ba3d25
Ashton B <ashtonbrugh@gmail.com>
2021-11-02 13:26:58 -0400

    styling the timer

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 var btnStart = document.getElementById("btn-start");
 var timer = document.getElementById("timer");
-var count = 4;
+var count = 12;
 
 btnStart.addEventListener('click', function() {
    var intervalId = setInterval(function () {
+
+    if(count <= 10){
+        document.getElementById("timer").style.background = "red";
+        document.getElementById("timer").style.color = "white";
+    }
 
     if(count === 0){
         clearInterval();
@@ -11,6 +16,8 @@ btnStart.addEventListener('click', function() {
     } else {
         count -= 1;
         timer.textContent = count; }
+
+    document.getElementById("timer").style.visibility = "visible";
         
     }, 1000)
 
