bd47c29bd845c67155fb67e50e717b952f55d324
Ashton B <ashtonbrugh@gmail.com>
2021-11-02 12:26:30 -0400

    add set and clear interval

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
+var btnStart = document.getElementById("btn-start");
+var timer = document.getElementById("timer");
+var count = 4;
 
-var questions = [
-{
-    question: "What does DOM stand for?",
-    choice1: "Data Optics Management",
-    choice2: "Document Object Model",
-    choice3: "Directionally Orchestrated Money",
-    choice4: "Dynamic Optional Modem",
-    answer: 2
-},
+btnStart.addEventListener('click', function() {
+   var intervalId = setInterval(function () {
 
-{
-    question: "What is NOT an example of a logical operator?",
-    choice1: "^^",
-    choice2: "&&",
-    choice3: "||",
-    choice4: "!",
-    answer: 1
-},
+    if(count === 0){
+        clearInterval();
 
-{
-    question: "What is NOT an example of a HTML DOM mouse event?",
-    choice1: "onclick",
-    choice2: "mousedown",
-    choice3: "mouseover",
-    choice4: "mickeymouse",
-    answer: 4
-},
-{
-    question: "Where does the Javascript tag go on your HTML page?",
-    choice1: "In the <head> under the stylesheet ref",
-    choice2: "In the <body> above the header",
-    choice3: "At the bottom of the <body>",
-    choice4: "After the </html> tag at the bottom of the page",
-    answer: 3
-}
-];
+    } else {
+        count -= 1;
+        timer.textContent = count; }
+        
+    }, 1000)
+
+    
+
+});
+
+    
+
+
+
+
+
+
+
+
 
 
 
