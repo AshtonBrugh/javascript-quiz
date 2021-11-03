84bea279dcf5bce7e06dffa5c0f9f301f0a2b1ca
Ashton B <ashtonbrugh@gmail.com>
2021-11-03 11:28:09 -0400

    test

--- script.js
+++ script.js
@@ -1,42 +1,7 @@
 var btnStart = document.getElementById("btn-start");
 var timer = document.getElementById("timer");
 var count = 60;
-var questions = [
-    {
-question: "What does DOM stand for?",
-choiceA: "Data Optics Management",
-choiceB: "Document Object Model",
-choiceC: "Directionally Orchestrated Money",
-choiceD: "Dynamic Optional Modem",
-correct: "B"
-},
 
-{
-question: "What is NOT an example of a logical operator?",
-choiceA: "^^",
-choiceB: "&&",
-choiceC: "||",
-choiceD: "!",
-correct: "A"
-},
-
-{
-question: "What is NOT an example of a HTML DOM mouse event?",
-choiceA: "onclick",
-choiceB: "mousedown",
-choiceC: "mouseover",
-choiceD: "mickeymouse",
-correct: "D"
-},
-{
-question: "Where does the Javascript tag go on your HTML page?",
-choiceA: "In the <head> under the stylesheet ref",
-choiceB: "In the <body> above the header",
-choiceC: "At the bottom of the <body>",
-choiceD: "After the </html> tag at the bottom of the page",
-correct: "C"
-}
-];
 
 //starting the timer
 btnStart.addEventListener('click', function() {
