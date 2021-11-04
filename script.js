456ae623c464c5736a076f053c8d56dc0f9ff66c
Ashton B <ashtonbrugh@gmail.com>
2021-11-04 11:39:04 -0400

    adding the multiple choice questions to html & css

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var btnStart = document.getElementById("btn-start");
 var timer = document.getElementById("timer");
-var count = 60;
+var count = 45;
+var tracker = ("turn this variable into an array of correct answers");
+
+                 
 
 
 //starting the timer
@@ -18,20 +21,115 @@ btnStart.addEventListener('click', function() {
     } else {
         count -= 1;
         timer.textContent = count; }
-
-    //timer appears
-    document.getElementById("timer").style.visibility = "visible";
-
-    //home page disappears
-    document.getElementById("home-page").style.display = "none";
-
-    //question one appears
-    var questionOne = document.getElementById("questions").style.display = "inherit";
-
     
     }, 1000)
 
+     //timer appears
+     document.getElementById("timer").style.visibility = "visible";
+
+     //home page disappears
+     document.getElementById("home-page").style.display = "none";
+ 
+     //question one appears
+     document.getElementById("question-one").style.display = "inherit";
+
 });
+
+document.querySelectorAll('.answersOne').forEach(item => {
+    item.addEventListener('click', event => {
+
+        document.getElementById("question-one").style.display = "none";
+        document.getElementById("question-two").style.display = "inherit";
+
+    })
+});
+
+document.querySelectorAll('.answersTwo').forEach(item => {
+    item.addEventListener('click', event => {
+
+        document.getElementById("question-two").style.display = "none";
+        document.getElementById("question-three").style.display = "inherit";
+
+    })
+});
+
+document.querySelectorAll('.answersThree').forEach(item => {
+    item.addEventListener('click', event => {
+
+        document.getElementById("question-three").style.display = "none";
+        document.getElementById("question-four").style.display = "inherit";
+
+    })
+});
+
+document.querySelectorAll('.answersFour').forEach(item => {
+    item.addEventListener('click', event => {
+
+        document.getElementById("question-four").style.display = "none";
+        document.getElementById("question-five").style.display = "inherit";
+
+    })
+});
+
+document.querySelectorAll('.answersFive').forEach(item => {
+    item.addEventListener('click', event => {
+
+        document.getElementById("question-five").style.display = "none";
+        document.getElementById("question-six").style.display = "inherit";
+
+    })
+});
+
+document.querySelectorAll('.answersSix').forEach(item => {
+    item.addEventListener('click', event => {
+
+        document.getElementById("question-six").style.display = "none";
+        document.getElementById("question-seven").style.display = "inherit";
+
+    })
+});
+
+document.querySelectorAll('.answersSeven').forEach(item => {
+    item.addEventListener('click', event => {
+
+        document.getElementById("question-seven").style.display = "none";
+        document.getElementById("question-eight").style.display = "inherit";
+
+    })
+});
+
+document.querySelectorAll('.answersEight').forEach(item => {
+    item.addEventListener('click', event => {
+
+        document.getElementById("question-eight").style.display = "none";
+        document.getElementById("question-nine").style.display = "inherit";
+
+    })
+});
+
+document.querySelectorAll('.answersNine').forEach(item => {
+    item.addEventListener('click', event => {
+
+        document.getElementById("question-nine").style.display = "none";
+        document.getElementById("question-ten").style.display = "inherit";
+
+    })
+});
+document.querySelectorAll('.answersTen').forEach(item => {
+    item.addEventListener('click', event => {
+
+        document.getElementById("question-ten").style.display = "none";
+
+    })
+});
+
+
+
+
+
+
+
+
 
 
 
